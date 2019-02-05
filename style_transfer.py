14853035d9adbd2c134997f5b55e2d9019469a51
Neo-The1 <rahul270890@gmail.com>
2019-02-04 19:39:09 -0500

    added optimization and output result display, but code needs debugging

--- style_transfer.py
+++ style_transfer.py
@@ -1,4 +1,5 @@
 import os
+import time
 import tensorflow as tf
 os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 import numpy as np
@@ -7,6 +8,8 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 mpl.rcParams['figure.figsize'] = (10,10)
 mpl.rcParams['axes.grid'] = False
+
+import IPython.display
 
 from PIL import Image
 
@@ -18,8 +21,8 @@ from tensorflow.python.keras import layers
 from tensorflow.python.keras import backend as K
 
 #Setup eager execution
-#tf.enable_eager_execution()
-#print("Eager execution: {}".format(tf.executing_eagerly()))
+tf.enable_eager_execution()
+print("Eager execution: {}".format(tf.executing_eagerly()))
 
 #set image and content images, include path if not in same directory as this script
 style_img = 'style.jpg'
@@ -64,7 +67,7 @@ def load_and_process_img(raw_img):
   return img_processed
 
 #to view images, we need to deprocess
-def deprocess(img_processed):
+def deprocess_img(img_processed):
     x = img_processed.copy()
     #squeeze batch dimension
     if len(x.shape) == 4:
@@ -74,7 +77,7 @@ def deprocess(img_processed):
     x[:,:,0] += 103.939
     x[:,:,1] += 116.779
     x[:,:,2] += 123.68
-    x = x[:,:,:,-1]
+    x = x[:, :, ::-1]
     
     x =np.clip(x,0,255).astype('uint8')
     return x
@@ -126,7 +129,7 @@ def get_style_loss(curr_style,target_style):
     normalization = 4.*(channels**2)*(width*height)**2
     gram_style = gram_matrix(curr_style)
     
-    return tf.reduce_mean(tf.square(gram_style - target_style))/normalization
+    return tf.reduce_mean(tf.square(gram_style - target_style))#/normalization
 
 
 
@@ -159,14 +162,14 @@ def get_feature_representations(model, content_img, style_img):
     
     return style_features, content_features
 
-def compute_loss(model, loss_weights, init_image, gram_style_features, content_features):
+def compute_loss(model, loss_weights, init_img, gram_style_features, content_features):
     """This function will compute the loss total loss.
     
     Arguments:
       model: The model that will give us access to the intermediate layers
       loss_weights: The weights of each contribution of each loss function. 
         (style weight, content weight, and total variation weight)
-      init_image: Our initial base image. This image is what we are updating with 
+      init_img: Our initial base image. This image is what we are updating with 
         our optimization process. We apply the gradients wrt the loss we are 
         calculating to this image.
       gram_style_features: Precomputed gram matrices corresponding to the 
@@ -182,32 +185,150 @@ def compute_loss(model, loss_weights, init_image, gram_style_features, content_f
     # Feed our init image through our model. This will give us the content and 
     # style representations at our desired layers. Since we're using eager
     # our model is callable just like any other function!
-    model_outputs = model(init_image)
+    model_outputs = model(init_img)
     
     style_output_features = model_outputs[:num_style_layers]
     content_output_features = model_outputs[num_style_layers:]
     
-    style_score = 0
-    content_score = 0
+    style_loss = 0
+    content_loss = 0
   
     # Accumulate style losses from all layers
     # Here, we equally weight each contribution of each loss layer
     weight_per_style_layer = 1.0 / float(num_style_layers)
     for target_style, comb_style in zip(gram_style_features, style_output_features):
-        style_score += weight_per_style_layer * get_style_loss(comb_style[0], target_style)
+        style_loss += weight_per_style_layer * get_style_loss(comb_style[0], target_style)
     
     # Accumulate content losses from all layers 
     weight_per_content_layer = 1.0 / float(num_content_layers)
     for target_content, comb_content in zip(content_features, content_output_features):
-        content_score += weight_per_content_layer* get_content_loss(comb_content[0], target_content)
+        content_loss += weight_per_content_layer* get_content_loss(comb_content[0], target_content)
   
-    style_score *= style_weight
-    content_score *= content_weight
+    style_loss *= style_weight
+    content_loss *= content_weight
   
     # Get total loss
-    loss = style_score + content_score 
-    return loss, style_score, content_score
+    loss = style_loss + content_loss
+    return loss, style_loss, content_loss
+
+def compute_grads(cfg):
+    #cfg is a dictionary with all required inputs, created in func perform_style_transfer
+    with tf.GradientTape() as tape:
+        all_loss =  compute_loss(**cfg)
+        #compute gradients wrto inout image
+        total_loss = all_loss[0]
     
+    return tape.gradient(total_loss,cfg['init_img']), all_loss
+    
+def perform_style_transfer(content_img,style_img,num_iterations = 1000,content_weight = 0,
+                           style_weight=100):
+    model = create_model()
+    #we are nto training the layers of vgg19, so set trainable= false
+    for layer in model.layers:
+        layer.trainable = False
+    
+    #Get content and style features
+    style_features,content_features = get_feature_representations(model,content_img,style_img)
+    gram_style_features = [gram_matrix(style_feature) for style_feature in style_features]
+    
+    #set initial image
+    init_img = load_and_process_img(content_img)
+    init_img = tfe.Variable(init_img,dtype=tf.float32)
+    
+    #create optimizer
+    optimizer = tf.train.AdamOptimizer(learning_rate=5, beta1=0.99, epsilon=0.1)
+    
+    #store the best result
+    best_loss, best_img = float('inf'), None
+    
+    #create the config
+    loss_weights = (style_weight,content_weight)
+    cfg = {
+        'model':model,
+        'loss_weights':loss_weights,
+        'init_img': init_img,
+        'gram_style_features': gram_style_features,
+        'content_features': content_features        
+            }
+    
+    #For display
+    num_rows = 2
+    num_cols = 5
+    display_interval = num_iterations/(num_rows*num_cols)
+    start_time = time.time()
+    global_start_time = time.time()
+    
+    norm_means = np.array([103.939, 116.779, 123.68])
+    min_vals = -norm_means
+    max_vals = 255 - norm_means
+    
+    imgs = []
+    
+    for i in range(num_iterations):
+        print(i)
+        grads,all_loss = compute_grads(cfg)
+        loss,style_loss,content_loss = all_loss
+        optimizer.apply_gradients([(grads, init_img)])
+        print(loss.numpy())
+        #clip the image values
+        clipped = []
+        for i in range(3):
+            clipped.append(tf.clip_by_value(init_img[:,:,:,i], min_vals[i], max_vals[i]))
+        
+        init_img.assign(tf.stack(clipped,axis=-1))
+        
+        if loss < best_loss:
+            #Update best loss and best image
+            best_loss = loss
+            #.numpy() gives concrete array
+            best_img = deprocess_img(init_img.numpy())
+            
+        if i % display_interval==0:
+            start_time = time.time()
+            plot_img = init_img.numpy()
+            plot_img = deprocess_img(plot_img)
+            imgs.append(plot_img)
+            IPython.display.clear_output(wait = True)
+            IPython.display.display_png(Image.fromarray(plot_img))
+            print('Iteration: {}'.format(i))
+            print('Total Loss: {:.4e},'
+                  'Style Loss: {:.4e},'
+                  'Content Loss: {:.4e},'
+                  'Time: {:.4f}s'.format(loss,style_loss,content_loss,time.time()-start_time))
+        
+    print('Total Time: {:.4f}s'.format(time.time()-global_start_time))
+    IPython.display.clear_output(wait=True)
+    plt.figure(figsize=(14,4))
+    
+    for i,img in enumerate(imgs):
+        plt.subplot(num_rows,num_cols,i+1)
+        plt.imshow(img)
+        plt.xticks([])
+        plt.yticks([])
+        
+    return best_img, best_loss
+    
+best_img,best_loss = perform_style_transfer(content_img,style_img,num_iterations=2)
+Image.fromarray(best_img)
+
+def show_results(best_img,content_img,style_img):
+    plt.figure(figsize=(10,5))
+    content = load_img(content_img)
+    style = load_img(style_img)
+    
+    plt.subplot(1, 2, 1)
+    imshow(content,'Content Image')
+    
+    plt.subplot(1, 2, 2)
+    imshow(style,'Style Image')
+    
+    plt.figure(figsize=(10,10))
+    plt.imshow(best_img)
+    plt.title('Output Image')
+    plt.show()
+
+show_results(best_img,content_img,style_img)    
+            
     
     
     
